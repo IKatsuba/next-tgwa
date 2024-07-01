8dc2577c8cec0d9f5549351060e929ec7b0f54e6
Igor Katsuba <igor@katsuba.dev>
2024-07-01 10:35:34 +0300

    add shadcn/ui

--- apps/bot/app/layout.tsx
+++ apps/bot/app/layout.tsx
@@ -1,8 +1,16 @@
 import './global.css';
 
+import { cn } from '@nextjs-telegram/utils';
+import { Inter as FontSans } from 'next/font/google';
+
 export const metadata = {
   title: 'Welcome to NextJS Telegram Bot!',
 };
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export default function RootLayout({
   children,
@@ -11,7 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- apps/bot/app/page.tsx
+++ apps/bot/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@nextjs-telegram/ui/button';
+
 export default function Index() {
   /*
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.tailwind file.
    */
-  return <div></div>;
+  return <Button>Some days</Button>;
 }
